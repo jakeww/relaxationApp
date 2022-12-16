e4f1179d9733b5afe5772b3d76b464bf18152df4
Jake Watembach <jakewatembach@gmail.com>
2022-12-16 13:32:31 -0700

    Initial Commit

---
+++ relaxation/relaxationApp.swift
@@ -0,0 +1,17 @@
+//
+//  relaxationApp.swift
+//  relaxation
+//
+//  Created by Jake Watembach on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct relaxationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
