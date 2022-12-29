ee2ae97361e1c47fefa84581d1096ae80b5ce47e
Jake Watembach <jakewatembach@gmail.com>
2022-12-28 23:28:25 -0700

    Minor changes throughout

--- relaxation/guidedMeditationView.swift
+++ relaxation/guidedMeditationView.swift
@@ -7,78 +7,94 @@
 
 import SwiftUI
 import AVFoundation
+import UIKit
+
 
 struct guidedMeditationView: View {
-    // The audio player
     @State private var audioPlayer: AVAudioPlayer?
-
-    // The state of the audio player
+    
     @State private var isPlaying: Bool = false
-
-    // The stored time of the audio player when the pause button is pressed
+    
     @State private var storedTime: TimeInterval = 0
-
+    
+    @State private var progress: Double = 0.0
+    
+    @State private var timer: Timer?
+    
     var body: some View {
-        VStack {
-            // The play button
-            Button(action: { self.play() }) {
-                Text("Play")
+        VStack{
+            HStack {
+                Button(action: { self.play() }) {
+                    Image(systemName: "play.square.fill")
+                        .font(.system(size: 50.0))
+                }
+                
+                Button(action: { self.pause() }) {
+                    Image(systemName: "pause.rectangle.fill")
+                        .font(.system(size: 50.0))
+                }
+                
+                Button(action: { self.stop() }) {
+                    Image(systemName: "xmark.square.fill")
+                        .font(.system(size: 50.0))
+                }
             }
-
-            // The pause button
-            Button(action: { self.pause() }) {
-                Text("Pause")
-            }
-
-            // The stop button
-            Button(action: { self.stop() }) {
-                Text("Stop")
-            }
+            ProgressView("Progress:", value: progress, total: 1.0)
+                .id(progress)
+                .frame(width: 200, height: 20)
+            
         }
     }
-
-    // Plays the guided meditation audio file
+        
     func play() {
-        // Locate the "Guided Meditation Compassion.m4a" file in the app's bundle
         guard let fileURL = Bundle.main.url(forResource: "Guided Meditation Compassion", withExtension: "m4a") else {
             print("Error finding audio file")
             return
         }
-
+        
         do {
-            // If the audio player is not yet initialized, create a new audio player with the file URL
             if audioPlayer == nil {
                 audioPlayer = try AVAudioPlayer(contentsOf: fileURL)
             }
-
-            // Set the current time of the audio player to the stored time
             audioPlayer?.currentTime = storedTime
-
-            // Play the audio
             audioPlayer?.play()
             isPlaying = true
+            
+            // Start the timer to update the progress bar
+            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+                self.updateProgress()
+            }
         } catch {
             print("Error playing audio: \(error)")
         }
     }
-
-    // Pauses the audio player
+    
     func pause() {
         audioPlayer?.pause()
         isPlaying = false
-
-        // Store the current time of the audio player
         storedTime = audioPlayer?.currentTime ?? 0
+        
+        // Invalidate the timer
+        timer?.invalidate()
     }
-
-    // Stops the audio player
+    
     func stop() {
         audioPlayer?.stop()
         audioPlayer = nil
         isPlaying = false
         storedTime = 0
+        
+        // Invalidate the timer
+        timer?.invalidate()
+    }
+    
+    func updateProgress() {
+        guard let audioPlayer = audioPlayer else { return }
+        progress = audioPlayer.currentTime / audioPlayer.duration
+        print("update progress")
     }
 }
+
 
 
 
